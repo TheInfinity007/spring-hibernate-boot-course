a02158320c37dfddeb15a419437102400f4cef97
Ram Chandra <46575254+TheInfinity007@users.noreply.github.com>
2025-07-21 00:55:28 +0530

    Add create students code

--- src/main/java/com/github/theinfinity007/springhibernatebootcourse/hibernateJpaCrud/StartupConfig.java
+++ src/main/java/com/github/theinfinity007/springhibernatebootcourse/hibernateJpaCrud/StartupConfig.java
@@ -1,5 +1,7 @@
 package com.github.theinfinity007.springhibernatebootcourse.hibernateJpaCrud;
 
+import com.github.theinfinity007.springhibernatebootcourse.hibernateJpaCrud.dao.StudentDAO;
+import com.github.theinfinity007.springhibernatebootcourse.hibernateJpaCrud.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,9 +11,37 @@ public class StartupConfig {
 
     // This command link runner will be executed after the spring beans have been loaded
     @Bean
-    public CommandLineRunner commandLineRunner(String[] args) {
+    public CommandLineRunner commandLineRunner(StudentDAO studentDao) {
         return runner -> {
-            System.out.println("Hello World");
+//            createStudent(studentDao);
+
+            createMultipleStudents(studentDao);
         };
+    }
+
+    private void createMultipleStudents(StudentDAO studentDAO) {
+        System.out.println("Creating three student objects ...");
+        Student newStudent1 = new Student("Vin", "Diesel", "vdiesel@yopmail.com");
+        Student newStudent2 = new Student("Michelle", "Rodriguez", "mrodriguez@yopmail.com");
+        Student newStudent3 = new Student("Jordana", "Brewster", "jbrewster@yopmail.com");
+
+        studentDAO.save(newStudent1);
+        studentDAO.save(newStudent2);
+        studentDAO.save(newStudent3);
+
+        System.out.println("Saved student.. ");
+    }
+
+    private void createStudent(StudentDAO studentDAO) {
+        // Create the student object
+        System.out.println("Creating new student object ...");
+        Student newStudent = new Student("Paul", "Walker", "pwalker@yopmail.com");
+
+        // save the student object
+        System.out.println("Saving the student ...");
+        studentDAO.save(newStudent);
+
+        // display id of the saved student
+        System.out.println("Saved student. Generated id: " + newStudent.getId());
     }
 }
