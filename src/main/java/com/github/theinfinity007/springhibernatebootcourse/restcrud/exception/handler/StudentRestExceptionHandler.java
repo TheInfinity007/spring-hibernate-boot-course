0e07b80c835e7bb8e9a174e29e9085f466e0ccbc
Ram Chandra <46575254+TheInfinity007@users.noreply.github.com>
2025-07-22 23:36:46 +0530

    Add global exception handler using @ControllerAdvice

--- src/main/java/com/github/theinfinity007/springhibernatebootcourse/restcrud/exception/handler/StudentRestExceptionHandler.java
+++ src/main/java/com/github/theinfinity007/springhibernatebootcourse/restcrud/exception/handler/StudentRestExceptionHandler.java
@@ -1,44 +1,15 @@
-package com.github.theinfinity007.springhibernatebootcourse.restcrud.controller;
+package com.github.theinfinity007.springhibernatebootcourse.restcrud.exception.handler;
 
-
-import com.github.theinfinity007.springhibernatebootcourse.hibernateJpaCrud.dao.StudentDAO;
-import com.github.theinfinity007.springhibernatebootcourse.hibernateJpaCrud.entity.Student;
+import com.github.theinfinity007.springhibernatebootcourse.restcrud.controller.StudentErrorResponse;
 import com.github.theinfinity007.springhibernatebootcourse.restcrud.exception.StudentNotFoundException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import java.util.List;
+@ControllerAdvice
+public class StudentRestExceptionHandler {
 
-@RestController
-@RequestMapping("/api/students")
-public class StudentRestController {
-
-    StudentDAO studentDao;
-
-    @Autowired
-    StudentRestController(StudentDAO studentDao){
-        this.studentDao = studentDao;
-    }
-
-    @GetMapping("")
-    public List<Student> getStudents() {
-        return studentDao.findAll();
-    }
-
-    @GetMapping("/{studentId}")
-    public Student getStudent(@PathVariable int studentId) {
-        Student student = studentDao.findById(studentId);
-
-        if (student == null) {
-            throw new StudentNotFoundException("StudentNotFoundException", "Student id not found - " + studentId);
-        }
-        return student;
-    }
-
-    /*
-    // Add an exception handler using @ExceptionHandler
     @ExceptionHandler
     public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException ex) {
         // create a student error response
@@ -51,9 +22,6 @@ public class StudentRestController {
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 
-    */
-
-    /*
     // Exception handler to catch any exception (catch all)
     @ExceptionHandler
     public ResponseEntity<StudentErrorResponse> handlerException(Exception ex){
@@ -67,5 +35,4 @@ public class StudentRestController {
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 
-    */
 }
