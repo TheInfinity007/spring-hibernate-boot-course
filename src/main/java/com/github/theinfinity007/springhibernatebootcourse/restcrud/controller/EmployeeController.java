a7561d25f3aa0cc489dc6f60aa261095b29c8c3a
Ram Chandra <46575254+TheInfinity007@users.noreply.github.com>
2025-07-24 00:17:48 +0530

    add route to patch employee

--- src/main/java/com/github/theinfinity007/springhibernatebootcourse/restcrud/controller/EmployeeController.java
+++ src/main/java/com/github/theinfinity007/springhibernatebootcourse/restcrud/controller/EmployeeController.java
@@ -1,11 +1,14 @@
 package com.github.theinfinity007.springhibernatebootcourse.restcrud.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.github.theinfinity007.springhibernatebootcourse.restcrud.entity.Employee;
 import com.github.theinfinity007.springhibernatebootcourse.restcrud.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 
 @RestController
@@ -13,10 +16,12 @@ import java.util.List;
 public class EmployeeController {
 
     private final EmployeeService employeeService;
+    private final ObjectMapper objectMapper;
 
     @Autowired
-    EmployeeController(EmployeeService employeeService) {
+    EmployeeController(EmployeeService employeeService, ObjectMapper objectMapper) {
         this.employeeService = employeeService;
+        this.objectMapper = objectMapper;
     }
 
     @GetMapping("")
@@ -50,6 +55,39 @@ public class EmployeeController {
         Employee updatedEmployee = employeeService.save(employee);
 
         return updatedEmployee;
+    }
+
+    @PatchMapping("/{employeeId}")
+    public Employee partialUpdateEmployee(@PathVariable int employeeId, @RequestBody Map<String, Object> payload) {
+        Employee employee = employeeService.findById(employeeId);
+
+        if (employee == null) {
+            throw new RuntimeException("Employee not found with id: " + employeeId);
+        }
+
+        // throw exception if the payload contains the id
+        if (payload.containsKey("id")) {
+            throw new RuntimeException("Employee id not allowed in the request body");
+        }
+
+        Employee patchedEmployee = apply(payload, employee);
+
+        patchedEmployee.setId(employeeId);
+
+         return employeeService.save(patchedEmployee);
+    }
+
+    private Employee apply(Map<String, Object> payload, Employee employee){
+        // convert the employee object to JSON object node
+        ObjectNode employeeNode = objectMapper.convertValue(employee, ObjectNode.class);
+
+        // convert the payload map to a JSON object node
+        ObjectNode patchNode = objectMapper.convertValue(payload, ObjectNode.class);
+
+        // merge the updates into the employee
+        employeeNode.setAll(patchNode);
+
+        return objectMapper.convertValue(employeeNode, Employee.class);
     }
 
 }
