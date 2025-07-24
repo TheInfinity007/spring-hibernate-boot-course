90c8abf5e9fdd67e36c66cabb0f379861a946d5c
Ram Chandra <46575254+TheInfinity007@users.noreply.github.com>
2025-07-25 00:33:53 +0530

    configure access based on the routes

--- src/main/java/com/github/theinfinity007/springhibernatebootcourse/restcrud/security/SecurityConfig.java
+++ src/main/java/com/github/theinfinity007/springhibernatebootcourse/restcrud/security/SecurityConfig.java
@@ -1,10 +1,15 @@
 package com.github.theinfinity007.springhibernatebootcourse.restcrud.security;
 
+import org.springframework.boot.actuate.autoconfigure.observation.ObservationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -22,5 +27,27 @@ public class SecurityConfig {
 
 
         return new InMemoryUserDetailsManager(john, mary, susan);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests( configurer ->
+                configurer
+                        .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT, "/api/employees/**").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PATCH, "/api/employees/**").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
+        );
+
+        // Use HTTP Basic authentication
+        http.httpBasic(Customizer.withDefaults());
+
+        // disable csrf
+        // in general, not required for stateless REST APIs that use POST, PUT, DELETE, patch
+        http.csrf(csrf -> csrf.disable());
+
+        return http.build();
     }
 }
