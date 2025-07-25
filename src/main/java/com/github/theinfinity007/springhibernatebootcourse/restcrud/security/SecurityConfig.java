03e8fb61af1893b616d1944faef83d4e9568793c
Ram Chandra <46575254+TheInfinity007@users.noreply.github.com>
2025-07-26 00:49:37 +0530

    Add support for JDBC for user basic auth

--- src/main/java/com/github/theinfinity007/springhibernatebootcourse/restcrud/security/SecurityConfig.java
+++ src/main/java/com/github/theinfinity007/springhibernatebootcourse/restcrud/security/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.github.theinfinity007.springhibernatebootcourse.restcrud.security;
 
-import org.springframework.boot.actuate.autoconfigure.observation.ObservationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -9,24 +8,20 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig {
 
+    // Add support for JDBC for the users basic authorization
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
-        UserDetails john = User.builder().username("john").password("{noop}test123")
-                .roles("EMPLOYEE").build();
-
-        UserDetails mary = User.builder().username("mary").password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER").build();
-
-        UserDetails susan = User.builder().username("susan").password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER", "ADMIN").build();
-
-
-        return new InMemoryUserDetailsManager(john, mary, susan);
+    public UserDetailsManager jdbcUserDetailsManager(DataSource datasource){
+        // telling spring boot to use the JDBC authentication with our data source
+        return new JdbcUserDetailsManager(datasource);
     }
 
     @Bean
@@ -50,4 +45,24 @@ public class SecurityConfig {
 
         return http.build();
     }
+
+    /*
+    // Setup in memory users details, commenting it as we are now using jdbc user and authorities tables now.
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+        UserDetails john = User.builder().username("john").password("{noop}test123")
+                .roles("EMPLOYEE").build();
+
+        UserDetails mary = User.builder().username("mary").password("{noop}test123")
+                .roles("EMPLOYEE", "MANAGER").build();
+
+        UserDetails susan = User.builder().username("susan").password("{noop}test123")
+                .roles("EMPLOYEE", "MANAGER", "ADMIN").build();
+
+
+        return new InMemoryUserDetailsManager(john, mary, susan);
+    }
+    */
+
+
 }
