dce49e0e73b5768e13abd9fca1848dd42f97ac8b
Ram Chandra <46575254+TheInfinity007@users.noreply.github.com>
2025-08-11 00:31:10 +0530

    Add the mvc controller for employees and list employees page

---
+++ src/main/java/com/github/theinfinity007/springhibernatebootcourse/restcrud/EmployeeMVCController.java
@@ -0,0 +1,31 @@
+package com.github.theinfinity007.springhibernatebootcourse.restcrud;
+
+import com.github.theinfinity007.springhibernatebootcourse.restcrud.entity.Employee;
+import com.github.theinfinity007.springhibernatebootcourse.restcrud.service.EmployeeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/mvc/employees")
+public class EmployeeMVCController {
+    private EmployeeService employeeService;
+
+    public EmployeeMVCController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/list")
+    public String listEmployees(Model model){
+        List<Employee> employees = employeeService.findAll();
+
+        model.addAttribute("employees", employees);
+
+        return "list-employees";
+    }
+
+
+}
